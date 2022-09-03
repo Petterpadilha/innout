26c210b26a419a211d6e45156d547cb635bdc2e1
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-03 18:50:51 -0400

    Tela de login

---
+++ src/views/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/phosphor-icons"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/styles/login.css">
+
+    <title>Innout| Ponto a ponto</title>
+</head>
+
+<body>
+    <form class="form-login" action="#" method="post">
+       
+        <div class="login-card card">
+        
+            <div class="card-header">
+                <i class="ph-bag-simple"></i>
+                <span class="font-weight-light m-2">In </span>
+                <span class="text2 font-weight-bold">N'</span>
+                <span class="font-weight-light m-2">Out</span>
+                <i class="ph-person-simple-run m-2"></i>
+            </div>
+            <div class="card-body">
+                <div class="form group">
+                <h1>Bem vindo</h1>
+                    <label for="email"> E-mail</label>
+                    <input type="email" id="email" name="email" class="form-control" placeholder="Digite seu email">
+                </div>
+                <div class="form group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Digite sua senha">
+                </div>
+            </div>
+            <div class="card-footer ">
+                    <button class="btn btn-lg btn-primary">Entrar</button>
+                </div>
+        </div>
+    </form>
+</body>
+
+</html>
