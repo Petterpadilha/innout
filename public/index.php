a6370698a9025ea159ff97a89ee00f940f07c019
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-03 14:57:13 -0400

    buscando dados do banco de dados

--- public/index.php
+++ public/index.php
@@ -2,4 +2,14 @@
 require_once('/home/petter-padilha/Projects/innout/Debug/vendor/autoload.php');
 require_once(dirname(__FILE__, 2) . '/src/config/database.php');
 
-Database::getConnection();
+$sql = "SELECT * FROM users";
+
+
+$result = Database::getResultFromQuery($sql);
+
+while($row = $result->fetch_assoc()) {
+        print_r($row);
+        echo '</br>';
+
+
+}
--- src/config/database.php
+++ src/config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+## criando a classe para a conexão
 class Database
 {
 
@@ -8,10 +9,21 @@ class Database
         $envPath = realpath(dirname(__FILE__) . '/../env.ini');
         $env = parse_ini_file($envPath);
         $conn = new mysqli($env['host'], $env['username'], $env['password'], $env['database']);
+
+## Criação da conexão com o banco de dados
         if ($conn->connect_error) {
             die("Erro: " . $conn->connect_error);
         }
 
         return $conn;
     }
+
+    public static function getResultFromQuery($sql) {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+        return $result;
+
+    }
+
 }
