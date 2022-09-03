80f5742f103914742437652aedcbba1c541b4619
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-03 17:35:11 -0400

    buscando todos os dados do banco de dados

--- public/index.php
+++ public/index.php
@@ -3,9 +3,7 @@ require_once('/home/petter-padilha/Projects/innout/Debug/vendor/autoload.php');
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'lucas', 'email'=> 'lucas@projeto.com']);
-// dd($user);
-
-$user->email = 'lucas@projeto.com.br';
-
-dd($user);
+foreach (User::get([], 'name') as $user) {
+    echo $user->name;
+    echo "</br>";
+}
--- src/models/User.php
+++ src/models/User.php
@@ -5,7 +5,7 @@ require_once(realpath(MODEL_PATH . '/Model.php'));
 
 class User extends Model
 {
-    protected static $tableName = 'users';
+    protected static $tableName = ' users';
     protected static $columns = [
         'id',
         'name',
