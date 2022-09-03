b7ffdc9080f024d4a704516402f21deae7d843f8
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-03 14:22:48 -0400

    config projeto

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once('/home/petter-padilha/Projects/innout/Debug/vendor/autoload.php');
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
 
-$var = 8;
-dd($var);
+Database::getConnection();
+
