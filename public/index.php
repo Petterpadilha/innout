a81a3a9029308a9837df76a196f2c85c24412006
Petter Padilha <petterson_sorriso@hotmail.com>
2022-09-03 16:07:09 -0400

    set e get refactore

--- public/index.php
+++ public/index.php
@@ -4,4 +4,8 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'lucas', 'email'=> 'lucas@projeto.com']);
+// dd($user);
+
+$user->email = 'lucas@projeto.com.br';
+
 dd($user);
